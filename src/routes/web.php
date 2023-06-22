8b33fee5c09ba9dc3e6a32987a12b18c860971b7
hgm_t3b <hgm@tiendas3b.com>
2023-06-22 12:20:54 -0600

    web

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response(['welcome'=> 'Pricecheck API v2 WEB']);
+    return response(['welcome'=> 'Pricecheck API v2 WEB odsfcjewdofwjhfwssssl']);
 });
